9e6130b2187903c94c1f99c412457edf8e19bb5a
Andrej-Marsic <andrej_marsic@mckinsey.com>
2024-06-09 16:43:30 +0200

    feat: update on spike

--- game_scanner/commands.py
+++ game_scanner/commands.py
@@ -1,3 +1,8 @@
+import os
+
+import requests
+from loguru import logger
+
 from game_scanner.add_wishlist import add_wishlist
 from game_scanner.db import retrieve_play_request
 from game_scanner.register_play import register_to_bgg
@@ -9,6 +14,7 @@ def spike_it(message_id):
     play_payload = PlayPayload(**data).model_dump()
     r = register_to_bgg(play_payload)
     message, _ = process_register_response(r)
+    update_my_board_games()
     return message
 
 
@@ -30,3 +36,19 @@ def set_it(message_id):
         f"You love [it](https://boardgamegeek.com/boardgame/{game_id}) now! ٩(ˊᗜˋ)و"
     )
     return message
+
+
+def update_my_board_games():
+    try:
+        github_token = os.getenv("GITHUB_TOKEN", "")
+        headers = {
+            "contentType": "application/json",
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": "token " + github_token,
+        }
+        data = {"event_type": "webhook"}
+        url = "https://api.github.com/repos/nraw/my_board_games/dispatches"
+        r = requests.post(url, headers=headers, json=data)
+        logger.info(f"Triggered update to my_board_games: {r.status_code}")
+    except:
+        logger.error("Failed to update my_board_games")
